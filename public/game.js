c4ec943bd925c26ce2418f854eeec7773c927fbb
dashkovskiyarsen00 <dashkovskiy.arsen00@gmail.com>
2025-12-28 23:08:06 +0000

    Replace Invicta text with logo image

--- public/game.js
+++ public/game.js
@@ -205,7 +205,7 @@ const prepareBoardTiles = () => {
       const brandText = tile.dataset.brandText || label;
       const brandLogoMarkup =
         brand === 'invicta'
-          ? '<img class="brand-logo brand-invicta" src="invicta-logo.png" alt="INVICTA">'
+          ? '<img class="brand-logo brand-invicta" src="invicta-logo.png" alt="">'
           : `<span class="brand-logo brand-${brand}">${brandText}</span>`;
       tile.innerHTML = `<span class="tile-label tile-label--brand">${brandLogoMarkup}</span><div class="tile-tokens"></div>`;
       return;
